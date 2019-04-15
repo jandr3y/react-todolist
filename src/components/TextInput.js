639c69c0d5fd16e2679643c669e707ef317ce95a
Lucas Jandrey <wdev.lucas@gmail.com>
2019-04-15 19:31:23 -0300

    Iniciando projeto de TODO List, base react

---
+++ src/components/TextInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class TextInput extends React.Component {
+  render(){
+    return (
+      <div>
+        <textarea rows={3} className="text-input">
+        </textarea>
+      </div>
+    )
+  }
+}
